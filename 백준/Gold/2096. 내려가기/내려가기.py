df33c52732b90580e3d548eb6323bb8c0018ff77
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-03-01 20:16:28 +0900

    [Gold V] Title: 내려가기, Time: 248 ms, Memory: 115324 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,11 +1,13 @@
-N = int(input())
+import sys
 
-a, b, c = map(int, input().split())
+N = int(sys.stdin.readline())
+
+a, b, c = map(int, sys.stdin.readline().split())
 mx1 = mn1 = a
 mx2 = mn2 = b
 mx3 = mn3 = c
 for _ in range(1, N):
-    a, b, c = map(int, input().split())
+    a, b, c = map(int, sys.stdin.readline().split())
     mx1, mx2, mx3 = max(mx2, mx1) + a, max(mx1, mx2, mx3) + b, max(mx3, mx2) + c
     mn1, mn2, mn3 = min(mn2, mn1) + a, min(mn1, mn2, mn3) + b, min(mn3, mn2) + c
 print(max(mx1, mx2, mx3), min(mn1, mn2, mn3))
