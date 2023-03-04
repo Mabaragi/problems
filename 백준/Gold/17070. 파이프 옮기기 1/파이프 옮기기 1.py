9f2cc55c5cc0ee080d8e13f7b8f39ea04fc51ede
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-03-05 00:28:17 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -1,19 +1,19 @@
 N = int(input())
 array = [list(map(int, input().split())) for _ in range(N)]
-ans = 0
+dp1 = [[0] * N for _ in range(N)]  # 가로인 상태의 경우의 수
+dp2 = [[0] * N for _ in range(N)]  # 세로인 상태의 경우의 수
+dp3 = [[0] * N for _ in range(N)]  # 대각선 상태의 경우의 수
+dp1[0][1] = 1  # 초기에 0,1 에 가로 상태로 놓임
+for j in range(2, N):
+    if array[0][j] == 0:
+        dp1[0][j] = dp1[0][j - 1]
 
+for i in range(1, N):
+    for j in range(1, N):
+        if array[i][j] == 0 and array[i-1][j] == 0 and array[i][j-1] == 0:
+            dp3[i][j] = dp3[i - 1][j - 1] + dp1[i - 1][j - 1] + dp2[i - 1][j - 1]
+        if array[i][j] == 0:
+            dp1[i][j] = dp3[i][j - 1] + dp1[i][j - 1]
+            dp2[i][j] = dp3[i-1][j] + dp2[i-1][j]
 
-def pipe(s, hi, hj):  # s 는 상태. 0은 가로, 1은 세로, 2는 대각
-    global ans
-    if hi == N - 1 and hj == N - 1:
-        ans += 1
-    if hi + 1 < N and hj + 1 < N and array[hi][hj + 1] != 1 and array[hi + 1][hj] != 1 and array[hi + 1][hj + 1] != 1:
-        pipe(2, hi + 1, hj + 1)  # 대각 이동
-    if s != 1 and hj + 1 < N and array[hi][hj + 1] != 1:
-        pipe(0, hi, hj + 1)  # 가로 이동
-    if s != 0 and hi + 1 < N and array[hi+1][hj] != 1:  # 세로로 놓인 상태
-        pipe(1, hi + 1, hj)  # 세로 이동
-
-
-pipe(0, 0, 1)
-print(ans)
+print(dp1[N-1][N-1] + dp2[N-1][N-1] + dp3[N-1][N-1])
