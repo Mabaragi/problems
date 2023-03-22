fcf85580cb0bb240e165dc119215184161057ecc
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-03-22 09:59:51 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 172 ms, Memory: 120676 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,16 +1,18 @@
 import sys
+from collections import deque
 
 N, K = map(int, sys.stdin.readline().split())
 
-qu = [(N, 0)]
+qu = deque([(N, 0)])
 
 visit = [100000] * 100001
 visit[N] = 0
 
 while qu:
-    cn, ci = qu.pop(0)
+    cn, ci = qu.popleft()
     for nn, ni in ((cn - 1, ci + 1), (cn + 1, ci + 1), (2 * cn, ci)):
         if (0 <= nn < 100001) and visit[nn] > ni:
             qu.append((nn, ni))
             visit[nn] = ni
+
 print(visit[K])
