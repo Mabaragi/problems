32f603dcd30bfc13589db8c818fc589ccd3aa1d5
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-04-05 15:26:54 +0900

    [Gold III] Title: 줄 세우기, Time: 292 ms, Memory: 123976 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -2,23 +2,24 @@ from collections import deque
 
 N, M = map(int, input().split())
 adjl = [[] for _ in range(N + 1)]
+in_degree = [0] * (N + 1)
 for _ in range(M):
     a, b = map(int, input().split())
     adjl[a].append(b)
+    in_degree[b] += 1
 visit = [0] * (N + 1)
-finished = [0] * (N + 1)
-T = deque([])
-
-
-def dfs(s):
-    visit[s] = 1
-    for e in adjl[s]:
-        if visit[e] == 0:
-            dfs(e)
-    T.appendleft(s)
-
-
+qu = deque([])
 for i in range(1, N + 1):
-    if visit[i] == 0:
-        dfs(i)
+    if in_degree[i] == 0:
+        qu.append(i)
+        visit[i] = 1
+T = []
+while qu:
+    cn = qu.popleft()
+    T.append(cn)
+    for nn in adjl[cn]:
+        if visit[nn] == 0:
+            in_degree[nn] -= 1
+            if in_degree[nn] == 0:
+                qu.append(nn)
 print(*T)
