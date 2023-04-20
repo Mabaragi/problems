a0429b2f6609eb774ebce01f03b6f3b23487923c
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-04-20 12:19:59 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -1,24 +1,25 @@
 import java.util.*;
-import java.io.FileInputStream;
+
 
 class Main
 {
 	public static void main(String args[]) throws Exception
 	{
-		Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     int N = sc.nextInt();
-    String ans = "";
-  
+    
+    StringBuilder sb = new StringBuilder();
     for (int i = 0; i < N; i++) {
-      for (int j = N - 1 - i; j > 0; j--) {
-        ans += ' ';
-      }
-      for (int j = 0; j < i + 1; j++) {
-        ans += '*';
-      }
-      if (i < N - 1) {
-        ans += '\n';  
-      }
+        for (int j = N - 1 - i; j > 0; j--) {
+            sb.append(' ');
+        }
+        for (int j = 0; j < i + 1; j++) {
+            sb.append('*');
+        }
+        if (i < N - 1) {
+            sb.append('\n');
+        }
     }
-    System.out.println(ans);
-}}
+    System.out.println(sb);
+}
+}
