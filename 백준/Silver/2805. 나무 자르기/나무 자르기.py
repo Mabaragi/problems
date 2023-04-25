d1b9a2dc1f8b50c3372c688cef7e0ffab4f97ef4
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-04-25 17:45:02 +0900

    [Silver II] Title: 나무 자르기, Time: 544 ms, Memory: 261196 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,13 +2,13 @@ N, K = map(int, input().split())
 trees = list(map(int, input().split()))
 left = 0
 right = max(trees)
-while left < right:
+while left <= right:
     mid = (left + right) // 2
     tree_amount = 0
     for tree in trees:
         tree_amount += max(0, tree - mid)
     if tree_amount < K:
-        right = mid
+        right = mid - 1
     else:
         left = mid + 1
         ans = mid
