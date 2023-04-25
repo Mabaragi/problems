c7acbfe7d3665f8896d05cf96ba1e80ae33deb9d
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-04-25 17:45:42 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 115176 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -4,13 +4,13 @@ lst = [int(input()) for _ in range(N)]
 left = 1
 right = max(lst)
 ans = 1
-while left < right:
-    middle = (left + right)//2 + 1
+while left <= right:
+    middle = (left + right)//2
     sm = 0
     for i in lst:
         sm += i//middle
     if sm >= K:
-        left = middle
+        left = middle + 1
         ans = middle
     else:
         right = middle - 1
