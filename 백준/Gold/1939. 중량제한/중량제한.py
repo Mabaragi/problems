5299c1141df083d03b27df20532ff54649e684ae
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-05-03 14:09:40 +0900

    [Gold III] Title: 중량제한, Time: 368 ms, Memory: 55828 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.py
+++ 백준/Gold/1939. 중량제한/중량제한.py
@@ -1,28 +1,39 @@
 import sys
-import heapq
+from collections import deque
+
 
 N, M = map(int, sys.stdin.readline().split())
-INF = 1e10
-d = [0] * (N + 1)
-
 adjl = [[] for _ in range(N + 1)]
 for _ in range(M):
     a, b, c = map(int, sys.stdin.readline().split())
     adjl[a].append((b, c))
     adjl[b].append((a, c))
+
 S, E = map(int, sys.stdin.readline().split())
-d[S] = INF
-heap = [(-INF, S)]
 
-while heap:
-    cl, cn = heapq.heappop(heap)
-    cl *= -1
-    if d[cn] > cl:
-        continue
-    for nn, nl in adjl[cn]:
-        limit_load = min(cl, nl)
-        if d[nn] < limit_load:
-            d[nn] = limit_load
-            heapq.heappush(heap, (-limit_load, nn))
 
-print(d[E])
+def bfs(load):
+    qu = deque([S])
+    visit = [0] * (N + 1)
+    visit[S] = 1
+    while qu:
+        curr = qu.popleft()
+        for nxt, nxt_limit_load in adjl[curr]:
+            if load <= nxt_limit_load and visit[nxt] == 0:
+                visit[nxt] = 1
+                if nxt == E:
+                    return True
+                qu.append(nxt)
+    return False
+
+
+left = 1
+right = int(1e9)
+while left <= right:
+    mid = (left + right) // 2
+    if bfs(mid):
+        ans = mid
+        left = mid + 1
+    else:
+        right = mid - 1
+print(ans)
