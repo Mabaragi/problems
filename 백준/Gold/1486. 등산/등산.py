74c90414c2a083afdf39d4afefb5d875c4f40e11
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-05-04 15:02:43 +0900

    [Gold II] Title: 등산, Time: 200 ms, Memory: 117580 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.py
+++ 백준/Gold/1486. 등산/등산.py
@@ -1,20 +1,11 @@
 from collections import deque
 
 
-def to_num(ch):
-    if ch.isupper():
-        return ord(ch) - ord('A')
-    else:
-        return ord(ch) - ord('a') + 26
+to_num = lambda ch: ord(ch) - ord('A') if ch.isupper() else ord(ch) - ord('a') + 26
 
 
 def get_max():
-    mx = 0
-    for i in range(N):
-        for j in range(M):
-            if visit_to[i][j] + visit_from[i][j] <= D and mx < array[i][j]:
-                mx = array[i][j]
-    return mx
+    return max([array[i][j] for i in range(N) for j in range(M) if visit_to[i][j] + visit_from[i][j] <= D])
 
 
 def bfs(drc):
@@ -25,17 +16,12 @@ def bfs(drc):
         ci, cj = qu.popleft()
         if visit[ci][cj] > D:
             continue
-        if drc == 1 and visit_to[ci][cj] + visit[ci][cj] > D:
-            continue
-        for di, dj in ((1, 0), (0, 1), (-1, 0), (0, - 1)):
+        for di, dj in ((1, 0), (0, 1), (-1, 0), (0, -1)):
             ni, nj = ci + di, cj + dj
             if 0 <= ni < N and 0 <= nj < M and abs(array[ni][nj] - array[ci][cj]) <= T:
-                if array[ni][nj] * drc < array[ci][cj] * drc and visit[ni][nj] > visit[ci][cj] + abs(
-                        array[ni][nj] - array[ci][cj]) ** 2:
-                    visit[ni][nj] = visit[ci][cj] + abs(array[ni][nj] - array[ci][cj]) ** 2
-                    qu.append((ni, nj))
-                elif array[ni][nj] * drc >= array[ci][cj] * drc and visit[ni][nj] > visit[ci][cj] + 1:
-                    visit[ni][nj] = visit[ci][cj] + 1
+                cost = (array[ni][nj] - array[ci][cj]) ** 2 if array[ni][nj] * drc < array[ci][cj] * drc else 1
+                if visit[ni][nj] > visit[ci][cj] + cost:
+                    visit[ni][nj] = visit[ci][cj] + cost
                     qu.append((ni, nj))
     return visit
 
