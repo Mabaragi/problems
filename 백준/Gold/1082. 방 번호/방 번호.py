9b372ddd5636e3f91bdd87a996e8e499082ee090
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-05-09 09:20:22 +0900

    [Gold III] Title: 방 번호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1082. 방 번호/방 번호.py
+++ 백준/Gold/1082. 방 번호/방 번호.py
@@ -1,43 +1,11 @@
-import sys
-
+INF = 1e6
 N = int(input())
-costs = list(map(int, input().split()))
+rooms = list(map(int, input().split()))
 M = int(input())
-lst = [(costs[N - 1], N - 1)]
-for i in range(N - 2, - 1, - 1):
-    if lst[-1][0] <= costs[i]:
-        continue
-    lst.append((costs[i], i))
+dp = [-INF] * (M + 1)
 
-if lst[-1][1] == 0 and len(lst) >= 2:
-    max_length = (M - lst[-2][0])//lst[-1][0] + 1
-else:
-    max_length = M//lst[-1][0]
+for i in range(N - 1, - 1, -1):
+    for j in range(rooms[i], M + 1):
+        dp[j] = max(dp[j - rooms[i]] * 10 + i, i, dp[j])
 
-dp = {}
-
-mx = 0
-def function(number, total, length):
-    global mx
-    # print(dp)
-    if number in dp and dp[number] >= total:
-        return
-    if total > M:
-        return
-    if length + (M - total)//lst[-1][0] < max_length:
-        return
-    mx = number
-    dp[number] = total
-    for i in range(len(lst)):
-        function(number + str(lst[i][1]), total + lst[i][0], length + 1)
-
-
-for i in range(len(lst)):
-    if lst[i][1] == 0:
-        continue
-    function(str(lst[i][1]), lst[i][0], 1)
-
-if dp:
-    print(max(map(int, dp.keys())))
-else:
-    print(0)
+print(dp[M])
