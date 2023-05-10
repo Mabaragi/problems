4e20a2a6f47819b8cfccbb726a95689e94a1236a
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-05-10 10:03:14 +0900

    [Gold II] Title: 반도체 설계, Time: 140 ms, Memory: 119656 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+lst = list(map(int, input().split()))
+A = [1e5] * N
+mx = 0
+for i in range(0, N):
+    left = 0
+    right = len(A) - 1
+    ans = - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if A[mid] >= lst[i]:
+            right = mid - 1
+        else:
+            left = mid + 1
+            ans = mid
+    A[ans + 1] = lst[i]
+    mx = max(mx, ans + 2)
+print(mx)
