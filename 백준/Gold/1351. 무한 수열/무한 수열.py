61f59811693d7564bbc911a6b649d649e33b6383
Mabaragi <122513704+Mabaragi@users.noreply.github.com>
2023-11-02 13:20:50 +0900

    [Gold V] Title: 무한 수열, Time: 132 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -5,9 +5,8 @@ import collections
 N, P, Q = map(int, input().split())
 
 precomp = collections.defaultdict(int)
+precomp[0] = 1
 def get_An(N):
-    if N == 0:
-        return 1
     if precomp[N] == 0:
         precomp[N] = get_An(math.floor(N/Q)) + get_An(math.floor(N/P))
     return precomp[N]
